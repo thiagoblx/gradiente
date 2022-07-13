dfe1a895936cd0a4cafb8f983258f602718e45c0
thiagoblx <99287193+thiagoblx@users.noreply.github.com>
2022-07-13 16:11:27 -0300

    Add files via upload

---
+++ gradiente1.0.f90
@@ -0,0 +1,19 @@
+program ando
+!Código feito para achar o gradiente de uma função específica num intevalo específico e exportar dados.
+!imagem [-2:2] 
+implicit none
+
+real:: fx, fy, f, cos, exp, u, dudx, dudy
+
+f = cos(exp(x**2 + y**2))
+
+u = x**2 + y**2
+
+
+
+
+
+
+
+stop
+end program
